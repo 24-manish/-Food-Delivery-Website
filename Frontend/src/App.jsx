43c2f3ec417b4b0f98e2cd41ace2740e0c0d50e3
24-manish <mastermanish24@gmail.com>
2024-06-10 21:37:32 +0530

    home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Navbar from './componets/navbar/navbar'
 const App = () => {
   return (
-    <div>
-      
+    <div className='w-[80%] m-auto ' >
+      <Navbar />
     </div>
   )
 }
---
+++ Frontend/src/componets/navbar/navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import {assets} from '../../assets/assets'
+const navbar = () => {
+
+    const [menu,setmenu]=useState("");
+    console.log(menu);
+    const menuItems = ["Home", "Menu", "Mobile App", "Contact Us"];
+
+
+
+
+  return (
+    <div className='pt-5 pl-0 flex justify-between items-center ' >
+      <img src={assets.logo} className='w-36' />
+     
+      <ul className="flex list-none gap-5 text-zinc-900 text-2xl">
+        {menuItems.map((item) => (
+          <li
+            key={item}
+            onClick={() => setmenu(item)}
+            className={`cursor-pointer py-2 px-4 rounded transition duration-300 ${
+              menu === item ? " border-b-[10px] border-b-orange-400 " : "hover:bg-orange-200 "
+            }`}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+     
+    
+
+      <div className="flex items-center gap-10 ">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button className="bg-transparent text-sm text-slate-700 border border-orange-600 py-3 px-8 rounded-full cursor-pointer hover:bg-orange-400 transition duration-500">
+  Sign In
+</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default navbar
